6876a65f50df46b728d5dd927def9adc624f8695
kerryhannigan <kerryhannigan@ufl.edu>
2021-10-19 08:57:09 -0400

    Initial commit of header component and form styling

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+const Home = () => {
+    return (
+        <div className="header">
+            <h2>Flight Search</h2>
+            <p>Choose your destinaton, or find cheap flights to anywhere</p>
+        </div>
+     );
+}
+ 
+export default Home;
