62a025dcdb3b37eaeac5698b31979e4194953e29
kerryhannigan <kerryhannigan@ufl.edu>
2021-11-19 12:12:05 -0500

    Separated components, refactoring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './CSS/App.css';
-import App from './App';
 import './CSS/index.css';
+import App from './App';
 
 
 ReactDOM.render(
